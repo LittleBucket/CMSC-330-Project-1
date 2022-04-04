cd29d7e1a70ece3e39d8ad76e9033c59f1a5524f
LittleBucket <100548231+LittleBucket@users.noreply.github.com>
2022-04-04 17:04:32 -0400

    update2

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,46 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+        //Main Frame
         JFrame frame = new JFrame("Calculator");
         frame.setSize(600,600);
         frame.setLocationRelativeTo(null);
         frame.setBackground(Color.gray);
         frame.setVisible(true);
-        frame.setLayout(new GridLayout(4,3,1,1));
+
+        //Button Frame
+        JPanel buttons = new JPanel();
+        buttons.setLayout(new GridBagLayout(4,3,5,5));
+        buttons.setVisible(true);
+        buttons.setSize(600,500);
+        buttons.setBackground(Color.BLACK);
+        buttons.setLocation(0,60);
+        JButton zero = new JButton();
+        zero.setText("0");
+        zero.setFont(new Font(null,Font.BOLD,60));
+        zero.setVisible(true);
+        zero.setSize(70,70);
+        JButton one = new JButton();
+        one.setText("1");
+        one.setVisible(true);
+        one.setSize(60,40);
+        JButton two = new JButton();
+        two.setText("2");
+        two.setVisible(true);
+        two.setSize(60,40);
+
+        JButton three = new JButton();
+        JButton four = new JButton();
+        JButton five = new JButton();
+        JButton six = new JButton();
+        JButton seven = new JButton();
+        JButton eight = new JButton();
+        JButton nine = new JButton();
+        buttons.add(zero);
+        buttons.add(one);
+
+        frame.add(buttons);
+        comps
 
     }
 }
